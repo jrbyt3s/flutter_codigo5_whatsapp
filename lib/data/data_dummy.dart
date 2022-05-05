b08530d0d2b61447cccd0d0bb6976f0a087b8553
jrbyt3s <josee.ramirezi@gmail.com>
2022-05-04 23:02:55 -0500

    add model chat

---
+++ lib/data/data_dummy.dart
@@ -0,0 +1,72 @@
+import 'package:flutter_codigo5_whatsapp/models/chat_model.dart';
+
+List<ChatModel> chatList = [
+  ChatModel(
+      avatarURL:
+          "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      username: "Ximena Lopez",
+      message: "Ya he enviado el encargo por correo",
+      time: "10:20 ",
+      countMessage: 5,
+      isTyping: false),
+  ChatModel(
+      avatarURL:
+          "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      username: "Juancarlos Lopez",
+      message: "Ya he enviado el encargo por correo",
+      time: "10:20 ",
+      countMessage: 5,
+      isTyping: true),
+  ChatModel(
+      avatarURL:
+          "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      username: "Miguel Saravia",
+      message: "Vamos a valiar el chuculum hoy en la noche, lleva a tus amigas",
+      time: "10:10 ",
+      countMessage: 5,
+      isTyping: false),
+  ChatModel(
+      avatarURL:
+          "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      username: "Carla Gonzales",
+      message: "Dejaste todo ordenado verdad?",
+      time: "08:21 ",
+      countMessage: 5,
+      isTyping: false),
+  ChatModel(
+    avatarURL:
+        "https://images.pexels.com/photos/1845534/pexels-photo-1845534.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    username: "Ximena Lopez",
+    message: "Ya he enviando el encargo por el courier",
+    time: "10:20",
+    countMessage: 1,
+    isTyping: false,
+  ),
+  ChatModel(
+    avatarURL:
+        "https://images.pexels.com/photos/4922971/pexels-photo-4922971.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    username: "Ludwig Lopez",
+    message: "Vi un tono muy bueno van a ir las amigas de miguel",
+    time: "12:40",
+    countMessage: 3,
+    isTyping: true,
+  ),
+  ChatModel(
+    avatarURL:
+        "https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    username: "Karla Gonzales",
+    message: "Flutter es genial!",
+    time: "15:22",
+    countMessage: 0,
+    isTyping: false,
+  ),
+  ChatModel(
+    avatarURL:
+        "https://images.pexels.com/photos/8422403/pexels-photo-8422403.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    username: "Daniel Montes Arias",
+    message: "Hola Amiga, cómo estas?",
+    time: "11:22",
+    countMessage: 1,
+    isTyping: true,
+  ),
+];
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo5_whatsapp/pages/chat_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -31,26 +32,36 @@ class _HomePageState extends State<HomePage>
         ],
         bottom: TabBar(
           controller: _tabController,
+          indicatorColor: Colors.white,
+          indicatorWeight: 3.5,
           tabs: [
             Tab(
               icon: Icon(Icons.camera_alt),
             ),
             Tab(
               child:
-                  Text("Chat", style: TextStyle(fontWeight: FontWeight.bold)),
+                  Text("CHATS", style: TextStyle(fontWeight: FontWeight.bold)),
             ),
             Tab(
               child:
-                  Text("Chat", style: TextStyle(fontWeight: FontWeight.bold)),
+                  Text("STATUS", style: TextStyle(fontWeight: FontWeight.bold)),
             ),
             Tab(
               child:
-                  Text("Chat", style: TextStyle(fontWeight: FontWeight.bold)),
+                  Text("CALL", style: TextStyle(fontWeight: FontWeight.bold)),
             ),
           ],
         ),
       ),
-      body: Container(),
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          Center(child: Text("Camara")),
+          ChatPage(),
+          Center(child: Text("Status")),
+          Center(child: Text("Calls")),
+        ],
+      ),
     );
   }
 }
