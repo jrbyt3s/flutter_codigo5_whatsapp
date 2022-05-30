ed573dd5e314aa3467836ded59952ede76e4000a
jrbyt3s <josee.ramirezi@gmail.com>
2022-05-30 18:57:59 -0500

    changes2

--- lib/pages/status_page.dart
+++ lib/pages/status_page.dart
@@ -8,11 +8,30 @@ class StatusPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-        itemCount: statusUsers.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ItemStatusWidget(statusModel: statusUsers[index]);
-        },
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 12),
+            padding: EdgeInsets.symmetric(vertical: 4),
+            child: Text(
+              "Recientes:",
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 13.0,
+                  color: Colors.black.withOpacity(0.55)),
+            ),
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: statusUsers.length,
+            itemBuilder: (BuildContext context, int index) {
+              return ItemStatusWidget(statusModel: statusUsers[index]);
+            },
+          ),
+        ],
       ),
     );
   }
